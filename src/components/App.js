c87de26350e5475181d677e19dcf8577c6d95f1d
Anoop Mundathan <anoop.mundathan@gmail.com>
2017-08-11 12:24:42 +0100

    Pass redux store as props to App

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,44 @@
 import React, { Component } from 'react'
-import fetchRecipes from '../utils/api'
+import { addRecipe } from '../actions'
 
 class App extends Component {
-    componentDidMount() {
-        fetchRecipes('bread');
+    
+    state = {
+        calendar: null
     }
+
+    componentDidMount() {
+        const { store } = this.props
+
+        store.subscribe(() => {
+            this.setState(() => {
+                calendar: store.getState()
+            })
+        })
+    }
+
+    submitFood = () => {
+        this.props.store.dispatch(addRecipe({
+            day: 'monday',
+            meal: 'breakfast',
+            recipe: {
+                label: this.input.value
+            }
+        }))
+        this.input.value = ''
+    }
+
     render() {
         return(
             <div>
-                Hello React
+                <input
+                    type="text"
+                    ref={(input) => this.input = input}
+                    placeholder="Monday's breakfast" />
+                <button onClick={this.submitFood}>Submit</button>
+                <pre>
+                    Monday's Breakfast: {this.state.calendar && this.state.calendar.monday.breakfast}
+                </pre>
             </div>
         )
     }
