b6c1494175d1bf96ea6a67f36f239e20d77bc75c
Anoop Mundathan <anoop.mundathan@gmail.com>
2017-08-13 10:42:33 +0100

    Map state to props

--- src/components/App.js
+++ src/components/App.js
@@ -4,10 +4,24 @@ import { connect } from 'react-redux'
 class App extends Component {
     
     render() {
+        console.log(this.props)
         return(
             <h1>Remove Vanilla redux</h1>
         )
     }
 }
 
-export default connect()(App)
+function mapStateToProps(calendar) {
+    const dayOrder = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday']
+    return {
+        calendar: dayOrder.map(day => ({
+            day, 
+            meals: Object.keys(calendar[day]).reduce((meals, meal) => {
+                meals = calendar[meal] ? calendar[meal] : null
+                return meals
+            }, {})
+        }))
+    }
+}
+
+export default connect(mapStateToProps)(App)
